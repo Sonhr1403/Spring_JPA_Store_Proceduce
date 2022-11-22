0ab8a6cd6a0f041e4197c847f878a0593ec7d93e
Sonhr1403 <sonhr1403@gmail.com>
2022-11-22 14:42:25 +0700

    Initial commit

---
+++ src/main/java/com/example/spring_jpa_store_proceduce/controller/CustomerController.java
@@ -0,0 +1,2 @@
+package com.example.spring_jpa_store_proceduce.controller;public class CustomerController {
+}
---
+++ src/main/java/com/example/spring_jpa_store_proceduce/service/ICustomerService.java
@@ -0,0 +1,2 @@
+package com.example.spring_jpa_store_proceduce.configuration;public interface ICustomerService {
+}
